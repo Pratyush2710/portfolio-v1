d0357165397f0c4d20ae76fce5a5cf239e696ed6
Pratyush Garg <pratyushhg@gmail.com>
2021-02-07 22:59:42 +0530

    Projects added to homepage

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,60 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Image from "gatsby-image"
-import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = () => {
-  return <h2>project copmonent</h2>
-}
+import React from "react";
+import PropTypes from "prop-types";
+import Image from "react-bootstrap/Image";
+import { FaGithubSquare, FaShareSquare } from "react-icons/fa";
+import apnaAnaaj from "./../assets/apnaAnaaj.png";
+import camscanner from "./../assets/camscanner.jpg";
+import gitGeek from "./../assets/gitGeek.gif";
 
-Project.propTypes = {}
+const Project = ({ title, description, image, github, url, stack, index }) => {
+  const mapImagesToProject = {
+    gitGeek: gitGeek,
+    apnaAnaaj: apnaAnaaj,
+    camscanner: camscanner,
+  };
+  console.log(
+    "hello",
+    mapImagesToProject,
+    typeof image,
+    mapImagesToProject[image]
+  );
+  return (
+    <article className="project">
+      <div
+        className="project-img"
+        style={{ position: "relative", overflow: "hidden" }}
+      >
+        <div aria-hidden="true" style={{ width: "100%" }}></div>
 
-export default Project
+        <Image
+          src={mapImagesToProject[image]}
+          // width="70%"
+          className="project-img"
+          style={{}}
+        />
+      </div>
+      <div className="project-info">
+        <span className="project-number">0 {index + 1}</span>
+        <h3>{title}</h3>
+        <p className="project-desc">{description}</p>
+        <div className="project-stack">
+          {stack.map((item, index) => {
+            return <span key={index}>{item}</span>;
+          })}
+        </div>
+        <div className="project-links">
+          <a href={github}>
+            <FaGithubSquare className="project-icon" />
+          </a>
+          <a href={url}>
+            <FaShareSquare className="project-icon" />
+          </a>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+Project.propTypes = {};
+
+export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,40 @@
-import React from "react"
-import Title from "./Title"
-import Project from "./Project"
-import { Link } from "gatsby"
-const Projects = () => {
-  return <h2>projects list</h2>
-}
+import React, { useEffect, useState } from "react";
+import Title from "./Title";
+import Project from "./Project";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Button } from "react-bootstrap";
+const Projects = ({ title, showLink }) => {
+  const [projects, setProjects] = useState({});
+  useEffect(() => {
+    axios
+      .get("data/projects.json")
+      .then((res) => {
+        console.log(res.data);
+        setProjects(res.data["projects"]);
+      }, [])
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-export default Projects
+  
+  return (
+    <section className="section projects">
+      <Title title={title} />
+      <div className="section-center projects-center">
+        {projects.length > 0 &&
+          projects.map((project, index) => {
+            return <Project key={project.id} index={index} {...project} />;
+          })}
+      </div>
+      {showLink && (
+        <a href="https://github.com/Pratyush2710">
+          <button className="btn center-btn">Projects on Github</button>
+        </a>
+      )}
+    </section>
+  );
+};
+
+export default Projects;
